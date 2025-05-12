5ed57681f563f23ebc140edd4229b1846f24c6ac
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2025-05-12 10:49:25 +0200

    update

--- run-macOS.sh
+++ run-macOS.sh
@@ -1,9 +1,13 @@
 docker run --name mb-gnuradio \
     -v "$(pwd)"/mount-gr_persistent:/home/gnuradio/persistent/ \
-    -v /Volumes/MBU-Drive/Radio-Exp-Rec/Radio-Exp-05-06-24/thomasRec:/home/gnuradio/tmpFiles/ \
     -it mb-gnuradio-image
 
 exit
+
+docker run --name mb-gnuradio \
+    -v "$(pwd)"/mount-gr_persistent:/home/gnuradio/persistent/ \
+    -v /Volumes/MBU-Drive/Radio-Exp-Rec/Radio-Exp-05-06-24/thomasRec:/home/gnuradio/tmpFiles/ \
+    -it mb-gnuradio-image
 
 docker run --name mb-gnuradio \
     -v "$(pwd)"/mount-gr_persistent:/home/gnuradio/persistent/ \
